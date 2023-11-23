956de45e97b59e4493cf7a4af7c10553c86cb245
EnzoFavret3il <favrete@3il.fr>
2023-11-23 10:34:14 +0100

    TEST1

---
+++ src/FizzTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+import junit.framework.Assert;
+public class FizzTest {
+	FizzBuzz fiz=new FizzBuzz();
+	public void Test1() {
+		Assert.assertEquals(fiz.Mot(4),"Fizz");
+	}
+}
